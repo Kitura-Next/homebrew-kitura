d5a0b76d08880f186038e858db3dbf60309eb287
solarise9 <solarise9@users.noreply.github.com>
2020-11-02 14:30:06 +0900

    update to version 0.2.2

--- kitura.rb
+++ kitura.rb
@@ -1,9 +1,9 @@
 class Kitura < Formula
   desc "Kitura command-line interface"
   homepage "https://github.com/Kitura-Next/kitura-cli#readme"
-  url "https://github.com/Kitura-Next/kitura-cli/releases/download/0.1.0/kitura-cli_0.1.0_darwin.tar.gz"
-  version "0.1.0"
-  sha256 "bf8c529cfbfb2fd8a02ba95ff9a1f557d98373ac573f632889a084f0926be844"
+  url "https://github.com/Kitura-Next/kitura-cli/releases/download/0.2.2/kitura-cli_0.2.2_darwin.tar.gz"
+  version "0.2.2"
+  sha256 "b21db99fa3ee99de03cc3768462b81a5428a5fff9844683cf81b85688120ea7a"
 
   def install
     bin.install "kitura"
